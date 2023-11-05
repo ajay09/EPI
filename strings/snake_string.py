fdac1e0eeb68e20426961004b7156c63b50074e2
ajay09 <ajay199109@gmail.com>
2023-11-05 18:34:38 +0000

    2 - snake_string.py

---
+++ strings/snake_string.py
@@ -0,0 +1,11 @@
+from test_framework import generic_test
+
+
+def snake_string(s: str) -> str:
+    return s[1::4] + s[0::2] + s[3::4]
+
+
+if __name__ == '__main__':
+    exit(
+        generic_test.generic_test_main('snake_string.py', 'snake_string.tsv',
+                                       snake_string))
