df005edf12fa6b8cda35f3d8eba4e649ffef9f7c
ajay09 <ajay199109@gmail.com>
2023-11-14 23:32:38 +0000

    3 - queue_from_stacks.py

---
+++ stacks_queues/queue_from_stacks.py
@@ -0,0 +1,44 @@
+from test_framework import generic_test
+from test_framework.test_failure import TestFailure
+
+
+class Queue:
+    def __init__(self):
+        self._st1 = []
+        self._st2 = []
+
+    def enqueue(self, x: int) -> None:
+        self._st1.append(x)
+
+    def dequeue(self) -> int:
+        if len(self._st2) == 0:
+            while len(self._st1) > 0:
+                self._st2.append(self._st1.pop())
+
+        return self._st2.pop()
+
+
+def queue_tester(ops):
+    try:
+        q = Queue()
+
+        for (op, arg) in ops:
+            if op == 'Queue':
+                q = Queue()
+            elif op == 'enqueue':
+                q.enqueue(arg)
+            elif op == 'dequeue':
+                result = q.dequeue()
+                if result != arg:
+                    raise TestFailure('Dequeue: expected ' + str(arg) +
+                                      ', got ' + str(result))
+            else:
+                raise RuntimeError('Unsupported queue operation: ' + op)
+    except IndexError:
+        raise TestFailure('Unexpected IndexError exception')
+
+
+if __name__ == '__main__':
+    exit(
+        generic_test.generic_test_main('queue_from_stacks.py',
+                                       'queue_from_stacks.tsv', queue_tester))
