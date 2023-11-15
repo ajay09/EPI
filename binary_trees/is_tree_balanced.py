0cca890734f016fa2c12eabda6762228b81369c4
ajay09 <ajay199109@gmail.com>
2023-11-15 00:34:20 +0000

    Add learning.

--- binary_trees/is_tree_balanced.py
+++ binary_trees/is_tree_balanced.py
@@ -1,19 +1,34 @@
+from typing import NamedTuple
+
 from binary_tree_node import BinaryTreeNode
 from test_framework import generic_test
 
 
+"""
+Keep the named tuple definition outside the final function, as keeping inside the function would increase
+the running time.
+If kept inside the Named Tuple class definition will be created everytime the function is called.
+"""
+
+
+class BalancedStatusWithHeight(NamedTuple):
+    is_balanced: bool
+    height: int
+
+
 def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
-    def helper(node: BinaryTreeNode) -> (bool, int):
+    def helper(node: BinaryTreeNode) -> BalancedStatusWithHeight:
         if node is None:
-            return True, 0
+            return BalancedStatusWithHeight(True, 0)
 
         is_left_balanced, left_height = helper(node.left)
         is_right_balanced, right_height = helper(node.right)
 
-        return (is_left_balanced and is_right_balanced and (abs(left_height - right_height) <= 1)), max(left_height,
-                                                                                                        right_height) + 1
+        return BalancedStatusWithHeight((is_left_balanced and is_right_balanced and
+                                        (abs(left_height - right_height) <= 1)), max(left_height, right_height) + 1)
 
-    return helper(tree)[0]
+
+    return helper(tree).is_balanced
 
 
 
