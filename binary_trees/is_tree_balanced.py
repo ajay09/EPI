e03556a578afa1de9270ba5983d5563ee0dbaf5d
ajay09 <ajay199109@gmail.com>
2023-11-15 00:37:15 +0000

    Add learning.

--- binary_trees/is_tree_balanced.py
+++ binary_trees/is_tree_balanced.py
@@ -8,6 +8,9 @@ from test_framework import generic_test
 Keep the named tuple definition outside the final function, as keeping inside the function would increase
 the running time.
 If kept inside the Named Tuple class definition will be created everytime the function is called.
+
+Also think of early termination! 
+Average time reduced from 33us to 10us
 """
 
 
@@ -22,11 +25,15 @@ def is_balanced_binary_tree(tree: BinaryTreeNode) -> bool:
             return BalancedStatusWithHeight(True, 0)
 
         is_left_balanced, left_height = helper(node.left)
+        # Early termination gives the least execution time.
+        if not is_left_balanced:
+            return BalancedStatusWithHeight(False, 0)
+
         is_right_balanced, right_height = helper(node.right)
+        if not is_right_balanced:
+            return BalancedStatusWithHeight(False, 0)
 
-        return BalancedStatusWithHeight((is_left_balanced and is_right_balanced and
-                                        (abs(left_height - right_height) <= 1)), max(left_height, right_height) + 1)
-
+        return BalancedStatusWithHeight((abs(left_height - right_height) <= 1), max(left_height, right_height) + 1)
 
     return helper(tree).is_balanced
 
