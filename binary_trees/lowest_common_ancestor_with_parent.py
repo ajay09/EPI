5e95d128a32392d3de46f2482d43b51628e9d343
ajay09 <ajay199109@gmail.com>
2023-11-15 02:48:35 +0000

    3 - lowest_common_ancestor_with_parent.py

---
+++ binary_trees/lowest_common_ancestor_with_parent.py
@@ -0,0 +1,50 @@
+import functools
+from typing import Optional
+
+from binary_tree_with_parent_prototype import BinaryTreeNode
+from test_framework import generic_test
+from test_framework.binary_tree_utils import must_find_node
+from test_framework.test_failure import TestFailure
+from test_framework.test_utils import enable_executor_hook
+
+
+def lca(node0: BinaryTreeNode, node1: BinaryTreeNode) -> Optional[BinaryTreeNode]:
+    def get_depth(node: BinaryTreeNode) -> int:
+        depth = 0
+        while node:
+            node = node.parent
+            depth += 1
+        return depth
+
+    node0_depth = get_depth(node0)
+    node1_depth = get_depth(node1)
+
+    far, close = (node0, node1) if (node1_depth < node0_depth) else (node1, node0)
+    dept_diff = abs(node0_depth - node1_depth)
+
+    for _ in range(dept_diff):
+        far = far.parent
+
+    while far is not close:
+        far = far.parent
+        close = close.parent
+
+    return far
+
+
+@enable_executor_hook
+def lca_wrapper(executor, tree, node0, node1):
+    result = executor.run(
+        functools.partial(lca, must_find_node(tree, node0),
+                          must_find_node(tree, node1)))
+
+    if result is None:
+        raise TestFailure('Result can\'t be None')
+    return result.data
+
+
+if __name__ == '__main__':
+    exit(
+        generic_test.generic_test_main('lowest_common_ancestor_with_parent.py',
+                                       'lowest_common_ancestor.tsv',
+                                       lca_wrapper))
