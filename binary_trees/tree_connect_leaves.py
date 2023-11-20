a608e6f35b2ea7157838b843b591d798f71f8405
ajay09 <ajay199109@gmail.com>
2023-11-20 02:31:18 +0000

    6 - tree_connect_leaves.py

---
+++ binary_trees/tree_connect_leaves.py
@@ -0,0 +1,39 @@
+import functools
+from typing import List
+
+from binary_tree_node import BinaryTreeNode
+from test_framework import generic_test
+from test_framework.test_failure import TestFailure
+from test_framework.test_utils import enable_executor_hook
+
+
+def create_list_of_leaves(tree: BinaryTreeNode) -> List[BinaryTreeNode]:
+    stack, result = [], []
+    node = tree
+
+    while node or stack:
+        while node:
+            stack.append(node)
+            node = node.left
+        node = stack.pop()
+        if node.left is None and node.right is None:
+            result.append(node)
+        node = node.right
+
+    return result
+
+
+@enable_executor_hook
+def create_list_of_leaves_wrapper(executor, tree):
+    result = executor.run(functools.partial(create_list_of_leaves, tree))
+
+    if any(x is None for x in result):
+        raise TestFailure('Result list can\'t contain None')
+    return [x.data for x in result]
+
+
+if __name__ == '__main__':
+    exit(
+        generic_test.generic_test_main('tree_connect_leaves.py',
+                                       'tree_connect_leaves.tsv',
+                                       create_list_of_leaves_wrapper))
